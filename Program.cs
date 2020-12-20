22b6e40d8aa82349b8cd054878ce31dd029385f8
Logan Hutcheson <loganchutcheson@gmail.com>
2020-12-19 20:23:23 -0500

    using JSON.NET to parse working API call

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+
+using Newtonsoft.Json.Linq;
 
 namespace StarCitizenProject
 {
@@ -6,7 +10,22 @@ namespace StarCitizenProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WebClient client = new WebClient();
+
+            // Add a user agent header in case the
+            // requested URI contains a query.
+
+            Stream data = client.OpenRead("https://api.starcitizen-api.com/f3336da3459e5ed3ca5ef7ddd556159b/v1/live/user/dymerz");
+            StreamReader reader = new StreamReader(data);
+            string s = reader.ReadToEnd();
+            JObject parsed = JObject.Parse(s);
+
+            foreach(var pair in parsed)
+            {
+                Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+            }
+            data.Close();
+            reader.Close();
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+Project is using Json.NET to parse the responses. To install with Visual Studio Code run terminal: dotnet add package Newtonsoft.Json     --  To install with Visual Code you can use the Nuget package searcher
