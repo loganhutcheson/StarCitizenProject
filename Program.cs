5041e7e60ef3d7d87059ff984db8f27e88561ca5
Logan Hutcheson <loganchutcheson@gmail.com>
2020-12-28 22:15:29 -0500

    Trying to parse JObject

--- Program.cs
+++ Program.cs
@@ -19,13 +19,50 @@ namespace StarCitizenProject
             StreamReader reader = new StreamReader(data);
             string s = reader.ReadToEnd();
             JObject parsed = JObject.Parse(s);
+            User tim = new User(parsed);
 
             foreach(var pair in parsed)
             {
+                // if(pair.Key == "data") {
+                //     //Console.WriteLine(pair.Value);
+                //     JObject innerParse = JObject.Parse(pair.Value.ToString());
+                //     foreach(var innerPair in innerParse) {
+                //         if(innerPair.key == "organization") {
+                //             Console.WriteLine(pair.Value);
+                //         }
+                //     }
+                // }
                 Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
             }
             data.Close();
             reader.Close();
         }
+    }
+
+
+    //Map Class as Entity in Database   
+    class User 
+    {
+        String name;
+
+        JObject response;
+
+        public User(JObject jo) {
+            setResponse(jo);
+        }
+
+
+        public void setResponse(JObject jo) {
+            this.response = jo;
+        }
+
+
+        public void setName() {
+            //this.name = response.getName("handle");
+
+            
+
+        }
+
     }
 }
